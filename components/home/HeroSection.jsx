f3eacca059ca2e6332e4bee2a8694e9c28e1ff87
Mark Estareja <mark.estareja.me@gmail.com>
2026-06-27 15:06:32 +0800

    Hero page design. Unfinished

--- components/home/HeroSection.jsx
+++ components/home/HeroSection.jsx
@@ -3,16 +3,16 @@ import Image from "next/image"
 
 export default function HeroSection() {
   return (
-    <div className="border">
+    <div className="border px-12 md:px-15 lg:px-30">
         <div>
             <div>
                 <div>
-                    <h1 className="text-6xl font-bold text-red-700">Mark Estareja</h1>
-                    <h2>WEB DEVELOPER | UI/UX DESIGNER | GRAPHIC DESIGNER</h2>
+                    <h1 className="text-6xl md:text-7xl lg:text-8xl font-bold text-center">Mark Estareja</h1>
+                    <h2 className="text-lg md:text-xl lg:text-2xl text-light-2 text-center">WEB DEVELOPER | UI/UX DESIGNER | GRAPHIC DESIGNER</h2>
                 </div>
-                <p>I design and develop responsive websites using modern web technologies, with a strong focus on performance, usability, and clean UI.</p>
+                <p className="text-center">I design and develop responsive websites using modern web technologies, with a strong focus on performance, usability, and clean UI.</p>
             </div>
-            <div>
+            <div className="flex flex-col items-center gap-6">
                 <Link href="#">Let's Work Together</Link>
                 <Link href="#">See My Projects</Link>
             </div>
